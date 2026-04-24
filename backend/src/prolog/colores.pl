8c0a1dbbaf4248bead7dd2b9e06ff6c406c82df2
Dilan2582 <descobarr9@miumg.edu.gt>
2026-04-23 21:27:21 -0600

    Videos restantes grupo Dilan

--- backend/src/prolog/colores.pl
+++ backend/src/prolog/colores.pl
@@ -10,6 +10,14 @@ signo(anaranjado, colores, 'COL009').
 signo(naranja, colores, 'COL009').
 signo(cafe, colores, 'COL010').
 signo(gris, colores, 'COL011').
+signo(violeta, colores, 'COL012').
+signo(lila, colores, 'COL013').
+signo(celeste, colores, 'COL014').
+signo(beige, colores, 'COL015').
+signo(dorado, colores, 'COL016').
+signo(corinto, colores, 'COL017').
+signo(plateado, colores, 'COL018').
+signo(bronce, colores, 'COL019').
 
 video_youtube('COL001', 'https://youtu.be/FqjmTmlw6co').
 video_youtube('COL002', 'https://youtu.be/7wngRlWkORg').
@@ -21,3 +29,11 @@ video_youtube('COL008', 'https://youtu.be/O8ZLxiwceYQ').
 video_youtube('COL009', 'https://youtu.be/3UhefSTZB-E').
 video_youtube('COL010', 'https://youtu.be/bfbp-IekUsU').
 video_youtube('COL011', 'https://youtu.be/w2QiWIsLEqA').
+video_youtube('COL012', 'https://youtu.be/Ha7nEJykL-s').
+video_youtube('COL013', 'https://youtu.be/DmGAeprD9Jg').
+video_youtube('COL014', 'https://youtu.be/2oYp43vO4UA').
+video_youtube('COL015', 'https://youtu.be/uilxA2mkdZI').
+video_youtube('COL016', 'https://youtu.be/UBg-vJwQdrc').
+video_youtube('COL017', 'https://youtu.be/rpp_5zP8G5w').
+video_youtube('COL018', 'https://youtu.be/j2B0bMogUmI').
+video_youtube('COL019', 'https://youtu.be/Ffc-DHBs62w').
