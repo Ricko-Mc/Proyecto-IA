0410f6a41c063324c14bad677a2fc7b1fa395d6c
Ricardo Galindo <elementalstudio58@gmail.com>
2026-04-24 02:39:23 -0600

    Juego de memoria y Dicc alimentos

--- backend/src/prolog/alimentos.pl
+++ backend/src/prolog/alimentos.pl
@@ -1,21 +1,49 @@
-signo(agua, alimentos, 'ALI001').
-signo(tamal, alimentos, 'ALI002').
-signo(cafe, alimentos, 'ALI003').
-signo(tortilla, alimentos, 'ALI004').
-signo(pan, alimentos, 'ALI005').
-signo(arroz, alimentos, 'ALI006').
-signo(queso, alimentos, 'ALI007').
-signo(huevo, alimentos, 'ALI008').
-signo(frijol, alimentos, 'ALI009').
-signo(chile, alimentos, 'ALI010').
+signo(aceite, alimentos, 'ALI001').
+signo(arroz_con_leche, alimentos, 'ALI002').
+signo(atol, alimentos, 'ALI003').
+signo(carne_de_cerdo, alimentos, 'ALI004').
+signo(chirmol, alimentos, 'ALI005').
+signo(fiambre, alimentos, 'ALI006').
+signo(fideos, alimentos, 'ALI007').
+signo(frijol, alimentos, 'ALI008').
+signo(gaseosa, alimentos, 'ALI009').
+signo(huevo, alimentos, 'ALI010').
+signo(kakik, alimentos, 'ALI011').
+signo(lechuga, alimentos, 'ALI012').
+signo(maiz, alimentos, 'ALI013').
+signo(naranja, alimentos, 'ALI014').
+signo(pan, alimentos, 'ALI015').
+signo(pepian, alimentos, 'ALI016').
+signo(pastel, alimentos, 'ALI017').
+signo(pizza, alimentos, 'ALI018').
+signo(sopa, alimentos, 'ALI019').
+signo(shuco, alimentos, 'ALI020').
+signo(tamal, alimentos, 'ALI021').
+signo(tomate, alimentos, 'ALI022').
+signo(torreja, alimentos, 'ALI023').
+signo(tortilla, alimentos, 'ALI024').
 
-video_youtube('ALI001', 'UruEbwWgZfE').
-video_youtube('ALI002', 'UruEbwWgZfE').
-video_youtube('ALI003', 'UruEbwWgZfE').
-video_youtube('ALI004', 'UruEbwWgZfE').
-video_youtube('ALI005', 'UruEbwWgZfE').
-video_youtube('ALI006', 'UruEbwWgZfE').
-video_youtube('ALI007', 'UruEbwWgZfE').
-video_youtube('ALI008', 'UruEbwWgZfE').
-video_youtube('ALI009', 'UruEbwWgZfE').
-video_youtube('ALI010', 'UruEbwWgZfE').
+video_youtube('ALI001', 'UruEbwWgZfE'). % aceite
+video_youtube('ALI002', 'UruEbwWgZfE'). % arroz_con_leche
+video_youtube('ALI003', 'UruEbwWgZfE'). % atol
+video_youtube('ALI004', 'UruEbwWgZfE'). % carne_de_cerdo
+video_youtube('ALI005', 'UruEbwWgZfE'). % chirmol
+video_youtube('ALI006', 'UruEbwWgZfE'). % fiambre
+video_youtube('ALI007', 'UruEbwWgZfE'). % fideos
+video_youtube('ALI008', 'UruEbwWgZfE'). % frijol
+video_youtube('ALI009', 'UruEbwWgZfE'). % gaseosa
+video_youtube('ALI010', 'UruEbwWgZfE'). % huevo
+video_youtube('ALI011', 'UruEbwWgZfE'). % kakik
+video_youtube('ALI012', 'UruEbwWgZfE'). % lechuga
+video_youtube('ALI013', 'UruEbwWgZfE'). % maiz
+video_youtube('ALI014', 'UruEbwWgZfE'). % naranja
+video_youtube('ALI015', 'UruEbwWgZfE'). % pan
+video_youtube('ALI016', 'UruEbwWgZfE'). % pepian
+video_youtube('ALI017', 'UruEbwWgZfE'). % pastel
+video_youtube('ALI018', 'UruEbwWgZfE'). % pizza
+video_youtube('ALI019', 'UruEbwWgZfE'). % sopa
+video_youtube('ALI020', 'UruEbwWgZfE'). % shuco
+video_youtube('ALI021', 'UruEbwWgZfE'). % tamal
+video_youtube('ALI022', 'UruEbwWgZfE'). % tomate
+video_youtube('ALI023', 'UruEbwWgZfE'). % torreja
+video_youtube('ALI024', 'UruEbwWgZfE'). % tortilla
