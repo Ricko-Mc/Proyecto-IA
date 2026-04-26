c69ee2ace93b4aedd1c52168d3364117ab3d2d29
Ricardo Galindo <elementalstudio58@gmail.com>
2026-04-26 12:14:00 -0600

    Cambios finales de alimentos e implementacion de todos los commit

--- backend/src/prolog/alimentos.pl
+++ backend/src/prolog/alimentos.pl
@@ -1,49 +1,41 @@
 signo(aceite, alimentos, 'ALI001').
-signo(arroz_con_leche, alimentos, 'ALI002').
 signo(atol, alimentos, 'ALI003').
 signo(carne_de_cerdo, alimentos, 'ALI004').
-signo(chirmol, alimentos, 'ALI005').
-signo(fiambre, alimentos, 'ALI006').
+
+
 signo(fideos, alimentos, 'ALI007').
 signo(frijol, alimentos, 'ALI008').
 signo(gaseosa, alimentos, 'ALI009').
 signo(huevo, alimentos, 'ALI010').
-signo(kakik, alimentos, 'ALI011').
+
 signo(lechuga, alimentos, 'ALI012').
-signo(maiz, alimentos, 'ALI013').
-signo(naranja, alimentos, 'ALI014').
+
 signo(pan, alimentos, 'ALI015').
 signo(pepian, alimentos, 'ALI016').
 signo(pastel, alimentos, 'ALI017').
 signo(pizza, alimentos, 'ALI018').
 signo(sopa, alimentos, 'ALI019').
 signo(shuco, alimentos, 'ALI020').
-signo(tamal, alimentos, 'ALI021').
-signo(tomate, alimentos, 'ALI022').
-signo(torreja, alimentos, 'ALI023'). 
+signo(tamal, alimentos, 'ALI021'). 
 signo(tortilla, alimentos, 'ALI024').
 
-video_youtube('ALI001', 'UruEbwWgZfE'). % aceite
-video_youtube('ALI002', 'UruEbwWgZfE'). % arroz_con_leche
+video_youtube('ALI001', 'https://youtu.be/LIevqr-Csg0?si=D8WwWUH-Ci5xt41K'). % aceite
+
 video_youtube('ALI003', 'UruEbwWgZfE'). % atol
-video_youtube('ALI004', 'UruEbwWgZfE'). % carne_de_cerdo
-video_youtube('ALI005', 'UruEbwWgZfE'). % chirmol
-video_youtube('ALI006', 'UruEbwWgZfE'). % fiambre
-video_youtube('ALI007', 'UruEbwWgZfE'). % fideos
-video_youtube('ALI008', 'UruEbwWgZfE'). % frijol
-video_youtube('ALI009', 'UruEbwWgZfE'). % gaseosa
-video_youtube('ALI010', 'UruEbwWgZfE'). % huevo
-video_youtube('ALI011', 'UruEbwWgZfE'). % kakik
-video_youtube('ALI012', 'UruEbwWgZfE'). % lechuga
-video_youtube('ALI013', 'UruEbwWgZfE'). % maiz
-video_youtube('ALI014', 'UruEbwWgZfE'). % naranja
-video_youtube('ALI015', 'UruEbwWgZfE'). % pan
-video_youtube('ALI016', 'UruEbwWgZfE'). % pepian
-video_youtube('ALI017', 'UruEbwWgZfE'). % pastel
-video_youtube('ALI018', 'UruEbwWgZfE'). % pizza
-video_youtube('ALI019', 'UruEbwWgZfE'). % sopa
-video_youtube('ALI020', 'UruEbwWgZfE'). % shuco
-video_youtube('ALI021', 'UruEbwWgZfE'). % tamal
-video_youtube('ALI022', 'UruEbwWgZfE'). % tomate
-video_youtube('ALI023', 'UruEbwWgZfE'). % torreja
-video_youtube('ALI024', 'UruEbwWgZfE'). % tortilla
+video_youtube('ALI004', 'https://youtu.be/baXCGBujTlE?si=bCD0mB7_XSXC0Jac'). % carne_de_cerdo
+
+video_youtube('ALI007', 'https://youtu.be/CHWzS0GoYyU?si=7jPOSdHrY4qDbAji'). % fideos
+video_youtube('ALI008', 'https://youtu.be/JwYp9GDb3JQ?si=SoPsRVY1bBdCudxw'). % frijol
+video_youtube('ALI009', 'https://youtu.be/bBl_wlpX5Kg?si=_64pmQgueRHvmrlb'). % gaseosa
+video_youtube('ALI010', 'https://youtu.be/camqd6bxNno?si=9JjAZlDLjSQ8IVlx'). % huevo
+
+video_youtube('ALI012', 'https://youtu.be/zOGyVUYDzdo?si=idsEmesk2OBJylej'). % lechuga
+
+video_youtube('ALI015', 'https://youtu.be/SXFnSmKR2_Y?si=82KUCwVX_GkLzQKV'). % pan
+
+video_youtube('ALI017', 'https://youtu.be/qYn2FFS87pM?si=Lit8UNA5Jkw4JXOb'). % pastel
+video_youtube('ALI018', 'https://youtu.be/cL4qAbU89tI?si=PRVefER3fPvjH0zk'). % pizza
+video_youtube('ALI019', 'https://youtu.be/sHgRhIDBckg?si=GKeh83BHPPP-1v4O'). % sopa
+video_youtube('ALI020', 'https://youtu.be/qpy5oojOsUQ?si=acKDYar1fQ9rWJNO'). % shuco
+video_youtube('ALI021', 'https://youtu.be/x2LECu0p_PU?si=FNpnmuYDwjhl5jPz'). % tamal
+video_youtube('ALI024', 'https://youtu.be/1uSXP_dbfGc?si=EyiQkLzm5NukULnC'). % tortilla
