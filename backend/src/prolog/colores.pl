832366c436f6eb05b14a2d272b34b0787810b5bd
soffch <achafollam@miumg.edu.gt>
2026-04-29 21:38:34 -0600

    update cambio videos Grupo Colores

--- backend/src/prolog/colores.pl
+++ backend/src/prolog/colores.pl
@@ -18,22 +18,28 @@ signo(dorado, colores, 'COL016').
 signo(corinto, colores, 'COL017').
 signo(plateado, colores, 'COL018').
 signo(bronce, colores, 'COL019').
+signo(fucsia, colores, 'COL020').
+signo(colores, colores, 'COL021').
+
 
 video_youtube('COL001', 'https://youtu.be/FqjmTmlw6co').
-video_youtube('COL002', 'https://youtu.be/7wngRlWkORg').
+video_youtube('COL002', 'https://youtu.be/LFLczA_zMak').
 video_youtube('COL003', 'https://youtu.be/Qb6KkPkAWFg').
 video_youtube('COL004', 'https://youtu.be/Djr1OnrkedM').
 video_youtube('COL005', 'https://youtu.be/je6eWvQ120Y').
+video_youtube('COL006', 'https://youtu.be/48hUtiFC0ZM').
 video_youtube('COL007', 'https://youtu.be/3kdl3UOBCko').
 video_youtube('COL008', 'https://youtu.be/O8ZLxiwceYQ').
 video_youtube('COL009', 'https://youtu.be/3UhefSTZB-E').
 video_youtube('COL010', 'https://youtu.be/bfbp-IekUsU').
-video_youtube('COL011', 'https://youtu.be/w2QiWIsLEqA').
-video_youtube('COL012', 'https://youtu.be/Ha7nEJykL-s').
+video_youtube('COL011', 'https://youtu.be/eZMIh11rRho').
+video_youtube('COL012', 'https://youtu.be/N2hyVNY0NSs').
 video_youtube('COL013', 'https://youtu.be/DmGAeprD9Jg').
-video_youtube('COL014', 'https://youtu.be/2oYp43vO4UA').
+video_youtube('COL014', 'https://youtu.be/gICBFKAJZlE').
 video_youtube('COL015', 'https://youtu.be/uilxA2mkdZI').
-video_youtube('COL016', 'https://youtu.be/UBg-vJwQdrc').
+video_youtube('COL016', 'https://youtu.be/x2AGnloMMyY').
 video_youtube('COL017', 'https://youtu.be/rpp_5zP8G5w').
-video_youtube('COL018', 'https://youtu.be/j2B0bMogUmI').
-video_youtube('COL019', 'https://youtu.be/Ffc-DHBs62w').
+video_youtube('COL018', 'https://youtu.be/4GMFlWg2aPA').
+video_youtube('COL019', 'https://youtu.be/xZ2TTiVd_Cw').
+video_youtube('COL020', 'https://youtu.be/aBk-DGcA-dk').
+video_youtube('COL021', 'https://youtu.be/XlO7m8jRhFI').
